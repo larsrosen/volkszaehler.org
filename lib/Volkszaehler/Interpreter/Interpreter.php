c5b12470a53ff394f0b956e4dc701ed23f1df031
andig <cpuidle@gmx.de>
2013-12-15 14:49:29 +0100

    Allow numeric timestamps

--- lib/Volkszaehler/Interpreter/Interpreter.php
+++ lib/Volkszaehler/Interpreter/Interpreter.php
@@ -474,22 +474,19 @@ abstract class Interpreter {
 	 * @link http://de3.php.net/manual/en/datetime.formats.php
 	 * @todo add millisecond resolution
 	 *
-	 * @param mixed $ts int or string to parse
+	 * @param mixed $string int, float or string to parse
 	 * @param float $now in ms since 1970
 	 * @return float
 	 */
-	protected static function parseDateTimeString($val) {
-		if (ctype_digit($val)) { // handling as ms timestamp
-			return (float) $val;
+	protected static function parseDateTimeString($string) {
+		if (is_numeric($string)) { // handling as ms timestamp
+			return (float) $string;
 		}
-		elseif (is_int($val)) { // allow numeric parameters
-			return $val;
-		}
-		elseif ($ts = strtotime($val)) {
+		elseif ($ts = strtotime($string)) {
 			return $ts * 1000;
 		}
 		else {
-			throw new \Exception('Invalid time format: \'' . $val . '\'');
+			throw new \Exception('Invalid time format: \'' . $string . '\'');
 		}
 	}
 
